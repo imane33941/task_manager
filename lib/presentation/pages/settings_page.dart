1d6f2f999353bd9f6ee047b880402d9706160c2d
bichon <imane.bichon@icloud.com>
2026-06-03 16:02:17 +0200

    Replace default counter test with placeholder

--- lib/presentation/pages/settings_page.dart
+++ lib/presentation/pages/settings_page.dart
@@ -1,7 +1,14 @@
-import 'package:flutter_test/flutter_test.dart';
+import 'package:auto_route/auto_route.dart';
+import 'package:flutter/material.dart';
 
-void main() {
-  test('placeholder test', () {
-    expect(1 + 1, 2);
-  });
+@RoutePage()
+class SettingsPage extends StatelessWidget {
+  const SettingsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Paramètres'),
+    );
+  }
 }
